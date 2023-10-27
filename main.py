4e8a311dc62b3dc61a83d41de8fdad6a7aa3e467
Harshroxnox <harshroxnox@gmail.com>
2023-10-27 17:55:38 +0530

    cleaned the code a little bit and deleted requirements.txt as of no use

--- main.py
+++ main.py
@@ -22,6 +22,19 @@ ACTIONS = [
 model = tf.keras.models.load_model("keras_load_model")
 
 
+# This function returns the model's predicted fluctuations for inputs rating of team A, B, difference and
+# which team won.
+def predict(rating_a, rating_b, rating_diff, team_A_wins):
+    rating_a = rating_a/800
+    rating_b = rating_b/800
+    rating_diff = rating_diff/800
+    state = [rating_a, rating_b, rating_diff, team_A_wins]
+    q_values = model.predict(np.array([state]))
+    action = np.argmax(q_values)
+    fluctuation = ((rating_diff * 800) / ACTIONS[action]) + 20
+    return fluctuation
+
+
 class InputData(BaseModel):
     teamA: float
     teamB: float
@@ -30,7 +43,7 @@ class InputData(BaseModel):
 
 
 @app.post('/')
-async def predict(data: InputData):
+async def prediction(data: InputData):
 
     rating_a = data.teamA
     rating_b = data.teamB
@@ -39,13 +52,7 @@ async def predict(data: InputData):
     else:
         rating_diff = rating_b - rating_a
 
-    rating_a = rating_a/800
-    rating_b = rating_b/800
-    rating_diff = rating_diff/800
-    state = [rating_a, rating_b, rating_diff, data.team_A_wins]
-    q_values = model.predict(np.array([state]))
-    action = np.argmax(q_values)
-    fluctuation = ((rating_diff * 800) / ACTIONS[action]) + 20
+    fluctuation = predict(rating_a, rating_b, rating_diff, data.team_A_wins)
 
     return {
         "prediction": fluctuation
